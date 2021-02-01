2539c88437228ca0d79193049e79ae4fb7b020c4
JP <jeremiah.richmond@midlandu.edu>
2021-01-31 19:47:57 -0600

    Initial commit

---
+++ app/src/main/java/edu/coe/richmond/testing10/MainActivity.kt
@@ -0,0 +1,37 @@
+package edu.coe.richmond.testing10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Button
+import android.widget.Switch
+import java.util.*
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    val langList = arrayListOf("C++", "Java", "Python", "JavaScript", "Kotlin")
+    val conceptList = arrayListOf("Loops", "Variables", "Syntax", "Conditional Statements", "Input/Output")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val langText: TextView = findViewById(R.id.langText)
+        val button: Button = findViewById(R.id.button)
+        val switch1: Switch = findViewById(R.id.switch1)
+        val conceptText: TextView = findViewById(R.id.conceptText)
+        button.setOnClickListener {
+            val random = Random()
+            val randomLang = random.nextInt(langList.count())
+            langText.text = langList[randomLang]
+            if (switch1.isChecked) {
+                val randomConcept = random.nextInt(conceptList.count())
+                conceptText.text = conceptList[randomConcept]
+            }
+        }
+        fun test(v: View) {
+
+        }
+    }
+}
